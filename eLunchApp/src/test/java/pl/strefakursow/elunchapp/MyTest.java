d39608ec6f983b95d2713424984b910696769f1b
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-19 22:08:36 +0200

    added test for builder

---
+++ eLunchApp/src/test/java/pl/strefakursow/elunchapp/MyTest.java
@@ -0,0 +1,27 @@
+package pl.strefakursow.elunchapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.strefakursow.elunchapp.DTO.TestAddressDto;
+
+@SpringBootTest
+public class MyTest {
+
+    @Test
+    void test1() {
+        TestAddressDto testAddressDto = new TestAddressDto();
+        testAddressDto.setCity("Warszawa");
+        testAddressDto.setStreet("Jana Pawła");
+        testAddressDto.setStreetNumber("25");
+
+        TestAddressDto testAddressDtobuilded = new TestAddressDto.Builder()
+                .setCity("Warszawa")
+                .setStreet("Jana Pawła")
+                .setStreetNumber("25")
+                .build();
+
+        Assertions.assertEquals(testAddressDto, testAddressDtobuilded);
+    }
+
+}
