d6e17f5433d868d90de90faccf9318c6dbe939dd
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-06-23 21:13:34 +0200

    put methods 1.

--- eLunchApp/src/main/java/pl/strefakursow/elunchapp/service/DiscountCodeServiceImpl.java
+++ eLunchApp/src/main/java/pl/strefakursow/elunchapp/service/DiscountCodeServiceImpl.java
@@ -5,13 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import pl.strefakursow.elunchapp.DTO.DiscountCodeDto;
-import pl.strefakursow.elunchapp.model.DeliveryAddress;
+import pl.strefakursow.elunchapp.DTO.RestaurantDto;
+import pl.strefakursow.elunchapp.DTO.UserDTO;
+import pl.strefakursow.elunchapp.model.DiscountCode;
+import pl.strefakursow.elunchapp.model.DiscountCodeBuilder;
+import pl.strefakursow.elunchapp.model.Restaurant;
+import pl.strefakursow.elunchapp.model.User;
 import pl.strefakursow.elunchapp.repo.DiscountCodeRepo;
 import pl.strefakursow.elunchapp.repo.RestaurantRepo;
 import pl.strefakursow.elunchapp.repo.UserRepo;
 import pl.strefakursow.elunchapp.utils.ConverterUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -31,25 +38,63 @@ public class DiscountCodeServiceImpl implements DiscountCodeService {
 
     @Override
     public List<DiscountCodeDto> getAll() {
-        return deliveryAddressRepo.findAll().stream()
+        return discountCodeRepo.findAll().stream()
                 .map(ConverterUtils::convert)
                 .collect(Collectors.toList());
     }
 
     @Override
     public void put(UUID uuid, DiscountCodeDto discountCodeDto) {
+        if (!Objects.equals(discountCodeDto.getUuid(), uuid)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
+        List<User> users = new ArrayList<>();
+        if (discountCodeDto.getUsersDto() != null) {
+            for (UserDTO userDTO : discountCodeDto.getUsersDto()) {
+                User user = userRepo.findByUuid(userDTO.getUuid())
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+                users.add(user);
+            }
+        }
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        if (discountCodeDto.getRestaurants() != null) {
+            for (RestaurantDto restaurantDto : discountCodeDto.getRestaurants()) {
+                Restaurant restaurant = restaurantRepo.findByUuid(restaurantDto.getUuid())
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+                restaurants.add(restaurant);
+            }
+        }
+
+        DiscountCode discountCode = discountCodeRepo.findByUuid(discountCodeDto.getUuid())
+                .orElseGet(() -> newDiscountCode(uuid));
+
+        discountCode.setCode(discountCodeDto.getCode());
+        discountCode.setDiscount(discountCodeDto.getDiscount());
+        discountCode.setDiscountUnit(discountCodeDto.getDiscountUnit());
+        discountCode.setPeriod(discountCodeDto.getPeriod());
+        discountCode.setUsers(users);
+        discountCode.setRestaurants(restaurants);
+
+        if (discountCode.getId() == null) {
+            discountCodeRepo.save(discountCode);
+        }
     }
 
     @Override
     public void delete(UUID uuid) {
-        DeliveryAddress deliveryAddress = deliveryAddressRepo.findByUuid(uuid)
+        DiscountCode discountCode = discountCodeRepo.findByUuid(uuid)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        deliveryAddressRepo.delete(deliveryAddress);
+        discountCodeRepo.delete(discountCode);
     }
 
     @Override
     public Optional<DiscountCodeDto> getByUuid(UUID uuid) {
         return discountCodeRepo.findByUuid(uuid).map(ConverterUtils::convert);
+    }
+
+    private DiscountCode newDiscountCode(UUID uuid) {
+        return new DiscountCodeBuilder().withUuid(uuid).build();
     }
 }
--- eLunchApp/src/main/java/pl/strefakursow/elunchapp/service/OpenTimeServiceImpl.java
+++ eLunchApp/src/main/java/pl/strefakursow/elunchapp/service/OpenTimeServiceImpl.java
@@ -1,11 +1,18 @@
 package pl.strefakursow.elunchapp.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import pl.strefakursow.elunchapp.DTO.OpenTimeDto;
+import pl.strefakursow.elunchapp.model.OpenTime;
+import pl.strefakursow.elunchapp.model.OpenTimeBuilder;
+import pl.strefakursow.elunchapp.model.Restaurant;
 import pl.strefakursow.elunchapp.repo.OpenTimeRepo;
 import pl.strefakursow.elunchapp.repo.RestaurantRepo;
+import pl.strefakursow.elunchapp.utils.ConverterUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,7 +33,26 @@ public class OpenTimeServiceImpl implements OpenTimeService {
 
     @Override
     public void put(UUID uuid, OpenTimeDto openTimeDto) {
+        if (!Objects.equals(openTimeDto.getUuid(), uuid)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
+        Restaurant restaurant = restaurantRepo.findByUuid(openTimeDto.getRestaurantDto().getUuid())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+
+        OpenTime openTime = openTimeRepo.findByUuid(openTimeDto.getUuid())
+                .orElseGet(() -> newOpenTime(uuid, restaurant));
+
+        if (!Objects.equals(openTime.getRestaurant().getUuid(), openTimeDto.getRestaurantDto().getUuid())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        openTime.setDayOfWeek(openTimeDto.getDayOfWeek());
+        openTime.setPeriodTime(ConverterUtils.convert(openTimeDto.getPeriodTimeDto()));
+
+        if (openTime.getId() == null) {
+            openTimeRepo.save(openTime);
+        }
     }
 
     @Override
@@ -37,5 +63,12 @@ public class OpenTimeServiceImpl implements OpenTimeService {
     @Override
     public Optional<OpenTimeDto> getByUuid(UUID uuid) {
         return Optional.empty();
+    }
+
+    private OpenTime newOpenTime(UUID uuid, Restaurant restaurant) {
+        return new OpenTimeBuilder()
+                .withUuid(uuid)
+                .withRestaurant(restaurant)
+                .build();
     }
 }
